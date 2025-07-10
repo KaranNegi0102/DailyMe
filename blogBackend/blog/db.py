a02721c4834cc8437dbbdd9e623ef170e5a6a013
KaranNegi0102 <karannegi0102@gmail.com>
2025-07-11 01:45:22 +0530

    requirement file txt

--- blogBackend/blog/db.py
+++ blogBackend/blog/db.py
@@ -6,13 +6,9 @@ load_dotenv()
 
 
 
-conn = psycopg2.connect(
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"),
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+conn = psycopg2.connect(DATABASE_URL)
 
 def get_cursor():
   return conn.cursor()
