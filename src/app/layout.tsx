8233a8ce31d040ea608bd4d05ad315d2149e37e0
KaranNegi0102 <karannegi0102@gmail.com>
2025-07-12 01:21:19 +0530

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import  Provider  from "@/app/redux/Provider";
+import { Analytics } from '@vercel/analytics/next';
 
 
 
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <Provider>
         {children}
+        <Analytics />
 
         </Provider>
       </body>
